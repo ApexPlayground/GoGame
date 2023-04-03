ca0b56914bc6bcd64e1e6b7018abee8a27a9c5a6
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-03 20:03:00 +0100

    main file

--- gameCode/__main__.py
+++ gameCode/__main__.py
@@ -1,7 +1,14 @@
+f# Import the necessary modules and libraries for creating a graphical user interface
 from PyQt6.QtWidgets import QApplication
-from go import Go
-import sys
 
+# Import the custom "Go" module which presumably contains the code for the game Go
+from go import Go
+
+# Create a new instance of the QApplication class
 app = QApplication([])
+
+# Create a new instance of the Go class, which presumably contains the game logic for Go
 myGo = Go()
+
+# Start the application's event loop and wait for it to finish
 sys.exit(app.exec())
